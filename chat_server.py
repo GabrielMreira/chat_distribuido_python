3bcb1c04f848a4b8dae053d1eb767c1e2d3bb071
Gabriel Moreira <107933147+GabrielMreira@users.noreply.github.com>
2025-06-09 21:05:34 -0300

    chat: criação do server-side

---
+++ chat_server.py
@@ -0,0 +1,50 @@
+import grpc
+from concurrent import futures
+import time
+import threading
+
+from pyexpat.errors import messages
+
+import chat_pb2
+import chat_pb2_grpc
+
+class ChatService(chat_pb2_grpc.ChatServiceServicer):
+    def __init__(self):
+        self.messages = []
+        self.user_stream = {}
+        self.lock = threading.Lock
+
+    def ReceiveMessages(self, request, context):
+        usuario = request.user
+        print(f"Usuario {usuario} conectado")
+        ultimo_index = 0
+
+        try:
+            while context.is_active():
+                while len(self.messages) > ultimo_index:
+                    mensagem = self.messages[ultimo_index]
+                    yield mensagem
+                    ultimo_index += 1
+                time.sleep(0.1)
+        except grpc.RpcError:
+            print(f"Conexão com o  {usuario} finalizada")
+
+    def SendMessage(self, request, context):
+        mensagem = request.message
+        print(f"Nova mensagem de {mensagem.user}: {mensagem.content}")
+
+        while self.lock:
+            self.messages.append(messages)
+
+        return chat_pb2.Empty()
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    chat_pb2_grpc.add_ChatServiceServicer_to_server(ChatService, server)
+    server.add_insecure_port('[::]:50051')
+    print("Servidor rotando da porta 50051...")
+    server.start()
+    server.wait_for_termination()
+
+if __name__ == '__main__':
+    serve()
